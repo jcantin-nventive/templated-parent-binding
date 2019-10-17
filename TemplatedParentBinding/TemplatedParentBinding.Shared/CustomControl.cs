e4c1d5efe757113e8165d96ff2b4f5bc71574e83
Julie Cantin <julie.cantin@nventive.com>
2019-10-17 10:54:47 -0400

    Setup demo for templated parent bug

---
+++ TemplatedParentBinding/TemplatedParentBinding.Shared/CustomControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace TemplatedParentBinding
+{
+	public partial class CustomControl : Control
+	{
+		public int CustomProperty
+		{
+			get { return (int)GetValue(CustomPropertyProperty); }
+			set { SetValue(CustomPropertyProperty, value); }
+		}
+		
+		public static readonly DependencyProperty CustomPropertyProperty =
+			DependencyProperty.Register("CustomProperty", typeof(int), typeof(CustomControl), new PropertyMetadata(0));
+	}
+}
